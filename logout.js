54ddd3d6c0bb44e7f3531f428707f5afa83266fe
elf-fic <narmin_ibrahimli@outlook.com>
2025-10-27 16:22:41 -0400

    logout update

--- logout.js
+++ logout.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const logoutBtn = document.getElementById("logoutBtn");
+
   if (logoutBtn) {
     logoutBtn.addEventListener("click", (e) => {
       e.preventDefault();
       // Clear current user
-      localStorage.removeItem("currentUser");
+      sessionStorage.removeItem("currentUser");
       // Redirect to home
       window.location.href = "home.html";
     });
